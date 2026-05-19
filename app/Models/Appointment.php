983960b983a733785cad332fece6fd2370fed501
Lucatj <lucadegroot2008@outlook.com>
2026-05-19 11:24:38 +0200

    Changed FK to medewerker_id from users table

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -7,24 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     protected $fillable = [
-        'user_id',  
+        'klant_id',
         'medewerker_id',
-        'service', 
+        'service',
         'appointment_date',
         'duration', // toegevoegd
         'notes',
         'status'
     ];
-    
+
     protected $casts = [
         'appointment_date' => 'datetime'
     ];
-    
-    public function user()
+
+    public function klant()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'klant_id');
     }
 
+    // Relatie naar de Medewerker (User)
     public function medewerker()
     {
         return $this->belongsTo(User::class, 'medewerker_id');
