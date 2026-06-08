4812d263b59eca72227c0d52a87496dfbe5b67e1
ArendvanHattem <144318745+ArendvanHattem@users.noreply.github.com>
2026-06-08 03:10:57 +0200

    Remove key:generate from deploy script (using env vars)

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,14 +2,5 @@
 set -e
 cd /var/www/html
 
-echo "Generating application key..."
-php artisan key:generate --force
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
 echo "Running migrations..."
 php artisan migrate --force
