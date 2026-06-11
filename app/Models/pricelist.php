6463737c447e268af66cb3c2d3d4235897ca9c98
ArendvanHattem <144318745+ArendvanHattem@users.noreply.github.com>
2026-06-11 19:41:24 +0200

    Rename pricelist.php to Pricelist.php for Linux case sensitivity

--- app/Models/pricelist.php
+++ app/Models/pricelist.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class pricelist extends Model
+class Pricelist extends Model
 {
     use HasFactory;
     protected $fillable = ['service', 'description', 'amount', 'category', 'duration'];
